fe7ef76006c9ec6d56994ebbea4935c03a772049
Roc4rdho <maestro.rocardho@gmail.com>
2014-09-11 00:03:23 +0100

    first commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculator;
+
+/**
+ *
+ * @author root
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculatorForm form = new CalculatorForm();
+        form.setVisible(true);
+    }
+    
+}
